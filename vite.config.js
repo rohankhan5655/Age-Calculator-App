586fd9edf2ecb20965f8ad704d6628fed9d32689
rohankhan5655 <133367913+rohankhan5655@users.noreply.github.com>
2024-08-25 00:31:06 -0700

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Age-Calculator-App/', // This should match your repository name
+
 })
+
